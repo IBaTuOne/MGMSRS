5c83758adc88a4e3e1ddae37d7b8b89609c63aa1
esenbatuhan <esenbatuhan@users.noreply.github.com>
2026-09-09 20:48:19 +0300

    feat: robots.txt, sitemap.xml ve canonical metadata ekle

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.manavgatgm-srs.com.tr'),
   title: "Manavgat Gençlik Merkezi",
   description: "Saha Randevu Sistemi MGMSRS",
   applicationName: "MGMSRS",
+  alternates: {
+    canonical: '/',
+  },
   openGraph: {
     title: "Manavgat Gençlik Merkezi",
     description: "Saha Randevu Sistemi MGMSRS",
     siteName: "MGMSRS",
+    url: 'https://www.manavgatgm-srs.com.tr',
     locale: "tr_TR",
     type: "website",
   },
